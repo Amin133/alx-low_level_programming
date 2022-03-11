33e2b483a60cb0c2f853e3733c2ba35ef5d85f88
Amin133 <umaraminaabdulmumin@gmial.com>
2022-03-11 06:33:28 -0800

    print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print lowwercase alphabet
+ *
+ * Description: print lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(viod)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	puchar(10);
+
+	return (0);
+}
