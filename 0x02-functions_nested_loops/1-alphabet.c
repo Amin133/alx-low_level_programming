7fe7ad29b6bdaf108934a85adbd4c98bcf8e5baf
Amin133 <umaraminaabdulmumin@gmial.com>
2022-03-15 07:05:23 -0700

    print alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,11 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+int main(void)
 {
-char i;
-for (i = 'a', i <= 'z'; i++)
-_putchar(i);
-_putchar('\n');
+	char i;
+	for (i = 'a', i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
+	return (0);
 }
