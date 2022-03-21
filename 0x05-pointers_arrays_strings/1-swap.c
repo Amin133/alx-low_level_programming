4d866df98163b8274844959c5d832fb0a22cfcd8
Amin133 <umaraminaabdulmumin@gmial.com>
2022-03-21 06:43:55 -0700

    swap two intergers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,19 +6,14 @@
  * @b: integer
  * Return: Always 0 (success)
  */
-void swap_int(int *a, int *b, a, b);
+void swap_int(int *a, int *b);
 {
-	int *a;
-	int *b;
+	int a;
+	int b;
 
 	a = 58;
 	b = 48;
 
 	int *a = &b;
 	int *b = &a;
-	printf("a=%d, b=%d\n", a, b);
-
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
 }
