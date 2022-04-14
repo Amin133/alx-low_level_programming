0d4d1faff59b4e04a59eed7e13c1f8a2ec59cfae
Amin133 <umaraminaabdulmumin@gmial.com>
2022-04-14 05:35:49 -0700

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,92 +1,52 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
 /**
- * print_char - print char
- * @args: list of arguments
- */
-
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - print int
- * @args: list of arguments
- */
-
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - print float
- * @args: list of arguments
- */
-
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - print string
- * @args: list of arguments
- */
-
-void print_string(va_list args)
-{
-	char *s = va_arg(args, char *);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
-}
-
-/**
- * print_all - prints anything*
- * @format: list of types of arguments
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i, j;
-	char *s1 = "";
-	char *s2 = ", ";
+	va_list valist;
+	unsigned int i = 0, middle = 0;
+	char *arg;
 
-	op_t ops[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
+	va_start(valist, format);
 
-	va_start(args, format);
-	i = 0;
-	while (format != NULL && format[i] != '\0')
+	while (format && format[i])
 	{
-		j = 0;
-		while (ops[j].c != '\0')
+		if (middle)
+			printf(", ");
+		switch (format[i])
 		{
-			if (ops[j].c == format[i])
+		case 'c':
+			printf("%c", va_arg(valist, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(valist, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double));
+			break;
+		case 's':
+			arg = va_arg(valist, char *);
+			if (arg)
 			{
-				printf("%s", s1);
-				ops[j].f(args);
-				s1 = s2;
+				printf("%s", arg);
+				break;
 			}
-			j++;
+			printf("%p", arg);
+			break;
+		default:
+			middle = 0;
+			i++;
+			continue;
 		}
+		middle = 1;
 		i++;
 	}
 	printf("\n");
-	va_end(args);
+	va_end(valist);
 }
