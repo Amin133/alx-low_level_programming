55e581204e88b7e440565d807bfb86d741e938cb
Amin133 <umaraminaabdulmumin@gmial.com>
2022-04-26 07:40:29 -0700

    fixed

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,28 +1,49 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint_safe - frees a listint_t list
- * @h: data type listint_t double pointer of head
- * Return: the size of the list that was free'd
+ * free_listint_safe - function that free a listint_t linked list
+ * @h: pointer to the beginning of linked list
+ * Return: the number of nodes in the list
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-	size_t counter = 0;
-	listint_t *safe_node = *h;
-	listint_t *tmp_node;
+	int i, flag = 0;
+	listint_t *slow, *fast, *delete;
 
 	if (!h)
 		return (0);
-	while (safe_node != NULL)
-	{
-		counter++;
-		tmp_node = safe_node;
-		safe_node = safe_node->next;
-		free(tmp_node);
 
-		if (tmp_node < safe_node)
-			break;
+	for (i = 0; *h && !flag; i++)
+	{
+		slow = *h;
+		fast = (**h).next;
+		while (slow != fast)
+		{
+			if (slow)
+				slow = (*slow).next;
+			if (fast)
+				fast = (*fast).next;
+			if (fast == *h)
+				flag = 1;
+			if (fast)
+				fast = (*fast).next;
+			if (fast == *h)
+				flag = 1;
+		}
+		delete = *h;
+		*h = (**h).next;
+		free(delete);
+	}
+
+	while (flag && *h != fast)
+	{
+		delete = *h;
+		i++;
+		*h = (**h).next;
+		free(delete);
 	}
 	*h = NULL;
-	return (counter);
+	return (i);
 }
